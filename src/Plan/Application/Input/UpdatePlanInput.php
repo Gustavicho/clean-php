4e6fa8ca595459ccc1c43fd9878d937c788cf4c8
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2025-04-16 18:12:46 -0400

    feat: implement UpdatePlan

--- src/Plan/Application/Input/UpdatePlanInput.php
+++ src/Plan/Application/Input/UpdatePlanInput.php
@@ -4,4 +4,11 @@ namespace DDD\Plan\Application\Input;
 
 final readonly class UpdatePlanInput
 {
+    public function __construct(
+        public string $id,
+        public ?string $name,
+        public ?float $price,
+        public ?string $duration
+    ) {
+    }
 }
--- src/Plan/Application/UpdatePlan.php
+++ src/Plan/Application/UpdatePlan.php
@@ -2,13 +2,49 @@
 
 namespace DDD\Plan\Application;
 
+use Brick\Money\Money;
 use DDD\Plan\Application\Input\UpdatePlanInput;
 use DDD\Plan\Application\Output\UpdatePlanOutput;
+use DDD\Plan\Domain\Duration;
+use DDD\Plan\Domain\Plan;
+use DDD\Plan\Infra\Repository\PlanRepositoryI;
 
 final class UpdatePlan
 {
+    public function __construct(
+        private readonly PlanRepositoryI $repo,
+    ) {
+    }
+
     public function execute(UpdatePlanInput $input): UpdatePlanOutput
     {
-        return new UpdatePlanOutput();
+        $plan = $this->repo->findById($input->id);
+        if (!$plan) {
+            throw new \DomainException('Plan not found');
+        }
+
+        $price = $input->price !== null
+            ? Money::of($input->price, 'BRL')
+            : $plan->price;
+
+        $duration = $input->duration !== null
+            ? Duration::fromString($input->duration)
+            : $plan->duration;
+
+        $newPlan = new Plan(
+            $plan->id,
+            $input->name,
+            $price,
+            $duration,
+        );
+
+        $this->repo->update($newPlan);
+
+        return new UpdatePlanOutput(
+            $plan->id,
+            $plan->name,
+            $plan->price,
+            $plan->duration,
+        );
     }
 }
