58dd50feb3a04385ed3be917d6933ba4508d1628
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2025-04-16 02:03:09 -0400

    improv: add auxiliar cmds and a prepare repositories to test

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,32 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Expectations
-|--------------------------------------------------------------------------
-|
-| When you're writing tests, you often need to check that values meet certain conditions. The
-| "expect()" function gives you access to a set of "expectations" methods that you can use
-| to assert different things. Of course, you may extend the Expectation API at any time.
-|
-*/
+use DDD\Plan\Infra\Repository\PlanRepositoryInMemory;
+use DDD\Subscription\Infra\Repository\SubscriptionRepositoryInMemory;
+use DDD\User\Infra\Repository\UserRepositoryInMemory;
 
-expect()->extend('toBeOne', function () {
-    return $this->toBe(1);
+beforeAll(function () {
+    $this->planRepository = new PlanRepositoryInMemory();
+    $this->SubscriptionRepository = new SubscriptionRepositoryInMemory();
+    $this->userRepository = new UserRepositoryInMemory();
 });
-
-/*
-|--------------------------------------------------------------------------
-| Functions
-|--------------------------------------------------------------------------
-|
-| While Pest is very powerful out-of-the-box, you may have some testing code specific to your
-| project that you don't want to repeat in every file. Here you can also expose helpers as
-| global functions to help you to reduce the number of lines of code in your test files.
-|
-*/
-
-function something()
-{
-    // ..
-}
