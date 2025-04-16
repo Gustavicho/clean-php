94a54a866256bd506e96ca4b657d26d71bb15ddf
Gustavo <gustavoooliveiradecarvalho@gmail.com>
2025-04-16 15:07:35 -0400

    improv: user createion tests

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -1,51 +1,50 @@
 <?php
 
 use DDD\User\Application\Input\RegisterUserInput;
+use DDD\User\Application\Output\RegisterUserOutput;
 use DDD\User\Application\RegisterUser;
 
-describe('User craetion related test', function () {
-    it('Should register a new User', function () {
+describe('User creation', function () {
+    it('should register a new user', function () {
         $repo = inMemoryRepository();
         $input = new RegisterUserInput(
             'Gustavo Oliveira',
-            'gustavo@test.com',
+            'gustavo@example.com'
         );
 
         $registerUser = new RegisterUser($repo->user);
         $output = $registerUser->execute($input);
 
-        expect($output)->toBeReadonly();
+        expect($output)->toBeInstanceOf(RegisterUserOutput::class);
+        expect($output->name)->toBe('Gustavo Oliveira');
+        expect($output->email->value)->toBe('gustavo@example.com');
     });
 
-    it('Should fail for invalid Email format', function () {
+    it('should fail for invalid email format', function () {
         $repo = inMemoryRepository();
         $input = new RegisterUserInput(
             'Gustavo Oliveira',
-            'gustavo.test.com',
+            'gustavo.test.com'
         );
 
         $registerUser = new RegisterUser($repo->user);
-        expect(
-            $registerUser->execute($input)
-        )->toThrow(\InvalidArgumentException::class);
+
+        expect(fn () => $registerUser->execute($input))
+            ->toThrow(\InvalidArgumentException::class);
     });
 
-    it('Should fail for Email already exist', function () {
+    it('should fail when email already exists', function () {
         $repo = inMemoryRepository();
         $input = new RegisterUserInput(
             'Gustavo Oliveira',
-            'gustavo.test.com',
+            'gustavo@example.com'
         );
 
         $registerUser = new RegisterUser($repo->user);
+        // First registration should succeed.
         $registerUser->execute($input);
 
-        expect(
-            $registerUser->execute($input)
-        )->toThrow(\DomainException::class);
+        expect(fn () => $registerUser->execute($input))
+            ->toThrow(\DomainException::class);
     });
-});
-
-test('Should update the user data', function () {
-
 });
